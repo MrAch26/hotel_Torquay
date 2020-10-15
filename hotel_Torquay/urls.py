f7bb3d0e5ba2af0053e451d06e630d3417f45911
daniel.ach <MrAch26@users.noreply.github.com>
2020-10-15 18:29:40 +0300

    ok

--- hotel_Torquay/urls.py
+++ hotel_Torquay/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('staff/', include('Staff.urls')),
-    path('', include('Visitors.urls')),
-    path('visitors/', include('Visitors.urls')),
-] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('staff/', include('Staff.urls')),
+                  path('', include('Visitors.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
