01ac8780408dacc21467b3d7aaebba4f847f152b
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-10-20 09:46:01 +0300

    changed gitignore to not include static

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+// Global Script
+// Sidebar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "4em";
+}
